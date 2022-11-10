9312e43e1fff8d3e9601078cb2bddc6e42a30e38
mr-erold <mmpterold@gmail.com>
2022-11-10 13:20:20 +0200

    Add header and button components with simple functionality

--- Angular/task-tracker/src/app/app.module.ts
+++ Angular/task-tracker/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap"
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
